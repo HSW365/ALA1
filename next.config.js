e238154787124ec78b25193f62bd590c4648fb7e
HSW365media <hoodstarent365@gmail.com>
2026-03-26 14:40:47 -0400

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,21 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  distDir: '.next',
   images: {
     unoptimized: true
-  },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'X-DNS-Prefetch-Control',
-            value: 'on'
-          }
-        ]
-      }
-    ]
   }
 }
 
