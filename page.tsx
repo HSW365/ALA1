34700e75c6d4a8b0f92e6d9d72c98dd1f72d7ec0
HSW365media <hoodstarent365@gmail.com>
2026-03-27 10:49:53 -0400

    Create Page component with styled main section

---
+++ page.tsx
@@ -0,0 +1,15 @@
+export default function Page() {
+  return (
+    <main style={{
+      background: "#0a0f1c",
+      color: "white",
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontSize: "2rem"
+    }}>
+      ALA1 Cyberpunk Claw 🚀 LIVE
+    </main>
+  );
+}
